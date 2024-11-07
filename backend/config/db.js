636e90fd48f0a6108c81e648250dcaf480128d74
Ayyadurai <127571175+ayyadurai-k@users.noreply.github.com>
2024-11-07 06:25:29 +0530

    changed error message in db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose"
 
 export const connectDatabase = ()=>{
     mongoose.connect(process.env.MONGO_DB_URI).then(()=>{
-        console.log("Database Is Connected Successfully...!");
+        console.log("Database Is Connected Successfully");
     }).catch(()=>{
-        console.log("Database Connection Failed...!");
+        console.log("Database Connection Failed");
     })
 }
